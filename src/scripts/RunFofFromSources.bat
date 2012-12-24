b7ecf4027cfe2a54de095a3b1d52e2d98d5d1d8c
John Stumpo <stump@jstump.com>
2012-12-24 12:29:30 -0500

    Replace repository contents with pointer to new location.

--- src/scripts/RunFofFromSources.bat
+++
@@ -1,8 +0,0 @@
-call ClearCompiledPycs
-python FoFiX.py
-pause
-rem - this new pause allows the game to restart for new settings 
-rem - - without deleting the compiled files.
-cd scripts
-call ClearCompiledPycs
-pause
